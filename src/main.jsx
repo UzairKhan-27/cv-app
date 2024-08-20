0d45be873d945fa2aa497dd5db6e732e7f6fc50d
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-20 23:14:36 +0500

    Call General Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { General } from "./components/General.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<General />
 	</StrictMode>,
 );
