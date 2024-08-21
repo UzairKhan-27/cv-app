d86075c9e9097127f63e5e7ce955dc4e9f51ad43
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-21 23:53:46 +0500

    Add submit button for general info

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,53 +1,97 @@
 import { useState } from "react";
+import "../styles/General.css";
 
 function General() {
-	const [submitStatus, setSubmitStatus] = useState(0);
+	const [submitStatus, setSubmitStatus] = useState(false);
+	const [inputs, setInputs] = useState({});
+
+	function handleChange(event) {
+		const name = event.target.name;
+		const value = event.target.value;
+		setInputs((values) => ({ ...values, [name]: value }));
+	}
 
 	function handleSubmitClick(event) {
 		event.preventDefault();
-		console.log("here");
-		setSubmitStatus(submitStatus + 1);
+		setSubmitStatus(true);
+	}
+	function handleEditClick() {
+		setSubmitStatus(false);
 	}
 
 	return (
 		<>
-			<h1>Personal Information</h1>
-			<form>
+			<form className={submitStatus ? "hidden" : "personal-info"}>
+				<h1>Personal Information</h1>
 				<label htmlFor="name">
 					Full Name
 					<input
-						onChange={handleSubmitClick}
+						onChange={handleChange}
 						required
 						name="name"
 						type="text"
+						value={inputs.name || ""}
 					/>
 				</label>
 				<br />
 				<label htmlFor="email">
 					Email
-					<input required name="email" type="email" />
+					<input
+						onChange={handleChange}
+						required
+						name="email"
+						type="email"
+						value={inputs.email || ""}
+					/>
 				</label>
 				<br />
 				<label htmlFor="contact">
 					Contact Number
-					<input required name="contact" type="tel" />
+					<input
+						onChange={handleChange}
+						required
+						name="contact"
+						type="tel"
+						value={inputs.contact || ""}
+					/>
 				</label>
 				<br />
 				<label htmlFor="city">
 					City
-					<input required name="city" type="text" />
+					<input
+						onChange={handleChange}
+						required
+						name="city"
+						type="text"
+						value={inputs.city || ""}
+					/>
 				</label>
 				<br />
 				<label htmlFor="country">
 					Country
-					<input required name="country" type="text" />
+					<input
+						onChange={handleChange}
+						required
+						name="country"
+						type="text"
+						value={inputs.country || ""}
+					/>
 				</label>
 				<br />
 				<button onClick={handleSubmitClick} type="submit">
 					Submit
 				</button>
 			</form>
-			<h3>{submitStatus}</h3>
+			{submitStatus && (
+				<>
+					<button onClick={handleEditClick}>Edit</button>
+					<h3>{inputs.name}</h3>
+					<h3>{inputs.age}</h3>
+					<h3>{inputs.email}</h3>
+					<h3>{inputs.city}</h3>
+					<h3>{inputs.country}</h3>
+				</>
+			)}
 		</>
 	);
 }
