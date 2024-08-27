ea5078f015fdb18cdad24a421970e245904d88ca
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-27 14:53:02 +0500

    Fill form with initial values

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
-import "../styles/General.css";
 /* eslint-disable react/prop-types */
 
 function General({ submitStatus, className }) {
-	const [inputs, setInputs] = useState({});
+	const [inputs, setInputs] = useState({
+		name: "Uzair Khan",
+		email: "m.uzairkhan1000@gmail.com",
+		contact: "0311-1111000",
+		city: "Karachi",
+		country: "Pakistan",
+	});
 
 	function handleChange(event) {
 		const name = event.target.name;
@@ -15,56 +20,58 @@ function General({ submitStatus, className }) {
 		<>
 			<div className={"general " + className}>
 				<h1>Personal Information</h1>
-				<label htmlFor="name">
-					Full Name
-					<input
-						onChange={handleChange}
-						required
-						name="name"
-						type="text"
-						value={inputs.name || ""}
-					/>
-				</label>
-				<label htmlFor="email">
-					Email
-					<input
-						onChange={handleChange}
-						required
-						name="email"
-						type="email"
-						value={inputs.email || ""}
-					/>
-				</label>
-				<label htmlFor="contact">
-					Contact Number
-					<input
-						onChange={handleChange}
-						required
-						name="contact"
-						type="tel"
-						value={inputs.contact || ""}
-					/>
-				</label>
-				<label htmlFor="city">
-					City
-					<input
-						onChange={handleChange}
-						required
-						name="city"
-						type="text"
-						value={inputs.city || ""}
-					/>
-				</label>
-				<label htmlFor="country">
-					Country
-					<input
-						onChange={handleChange}
-						required
-						name="country"
-						type="text"
-						value={inputs.country || ""}
-					/>
-				</label>
+				<div className="general-list">
+					<label htmlFor="name">
+						Full Name
+						<input
+							onChange={handleChange}
+							required
+							name="name"
+							type="text"
+							value={inputs.name || ""}
+						/>
+					</label>
+					<label htmlFor="email">
+						Email
+						<input
+							onChange={handleChange}
+							required
+							name="email"
+							type="email"
+							value={inputs.email || ""}
+						/>
+					</label>
+					<label htmlFor="contact">
+						Contact Number
+						<input
+							onChange={handleChange}
+							required
+							name="contact"
+							type="tel"
+							value={inputs.contact || ""}
+						/>
+					</label>
+					<label htmlFor="city">
+						City
+						<input
+							onChange={handleChange}
+							required
+							name="city"
+							type="text"
+							value={inputs.city || ""}
+						/>
+					</label>
+					<label htmlFor="country">
+						Country
+						<input
+							onChange={handleChange}
+							required
+							name="country"
+							type="text"
+							value={inputs.country || ""}
+						/>
+					</label>
+				</div>
 			</div>
 
 			{submitStatus && (
